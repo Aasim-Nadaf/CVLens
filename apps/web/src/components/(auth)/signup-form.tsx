94bcacdd38dcdb574cf53efeef953120b9a087d3
Aasim Nadaf <nadafaasim@gmail.com>
2026-09-13 13:01:26 +0530

    feat: add sign-in and sign-up pages with corresponding forms

---
+++ apps/web/src/components/(auth)/signup-form.tsx
@@ -0,0 +1,104 @@
+import { cn } from "cn";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+              <CardHeader className="text-center">
+                <CardTitle className="text-xl">Create your account</CardTitle>
+                <CardDescription>
+                  Enter your email below to create your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form>
+                  <FieldGroup>
+                    <Field>
+                      <FieldLabel htmlFor="name">Full Name</FieldLabel>
+                      <Input
+                        id="name"
+                        type="text"
+                        placeholder="John Doe"
+                        required
+                      />
+                    </Field>
+                    <Field>
+                      <FieldLabel htmlFor="email">Email</FieldLabel>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </Field>
+                    <Field>
+                      <Field className="grid grid-cols-2 gap-4">
+                        <Field>
+                          <FieldLabel htmlFor="password">Password</FieldLabel>
+                          <Input id="password" type="password" required />
+                        </Field>
+                        <Field>
+                          <FieldLabel htmlFor="confirm-password">
+                            Confirm Password
+                          </FieldLabel>
+                          <Input
+                            id="confirm-password"
+                            type="password"
+                            required
+                          />
+                        </Field>
+                      </Field>
+                      <FieldDescription>
+                        Must be at least 8 characters long.
+                      </FieldDescription>
+                    </Field>
+                    <Field>
+                      <Button type="submit">Create Account</Button>
+                      <FieldDescription className="text-center">
+                        Already have an account? <a href="/sign-in">Sign in</a>
+                      </FieldDescription>
+                    </Field>
+                  </FieldGroup>
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="/placeholder.svg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </div>
+  );
+}
